79a64130859fc9776063eb7d995feb4f16b10520
nutsachichilidze <nchichilidze@gmail.com>
2022-03-21 18:16:14 +0000

    added unit testing  + changed directory structure

--- src/Booking/test_booking.java
+++
@@ -1,9 +0,0 @@
-package Booking;
-
-public class test_booking {
-    public static void main(String[] args) {
-        // Booking booking;
-        // EarlyBooking earlyBooking = new EarlyBooking();
-        // earlyBooking.createBooking();
-    }
-}
