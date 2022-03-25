21bd610934666e07b07cfc90c6e611b85d472875
eronaaliu <50829986+eronaaliu@users.noreply.github.com>
2022-03-25 11:13:03 +0000

    fixed enum

--- src/main/java/Reservation/ReservationType_ENUM.java
+++ src/main/java/Reservation/ReservationType_ENUM.java
@@ -1,11 +1,11 @@
 package Reservation;
 
 public enum ReservationType_ENUM {
-    STANDARD_RESERVATION("Standard Reservation"),
+    STANDARD_RESERVATION("Standard Reservation");
 
     public final String label;
 
-    private Element(String label) {
+    ReservationType_ENUM(String label) {
         this.label = label;
     }
 }
