dca558fb829128e54b051c305529cc344340ea66
nutsachichilidze <nchichilidze@gmail.com>
2022-04-04 17:30:01 +0100

    fixing tests

--- src/test/java/InterceptorTest.java
+++ src/test/java/InterceptorTest.java
@@ -11,6 +11,7 @@ public class InterceptorTest {
     public void testNextInChain() {
         loggingInterceptor logging = new loggingInterceptor("log");
         loggingInterceptor nextInChain = new loggingInterceptor("log2");
+        logging.setNextChain(nextInChain);
         interceptor actual_nextInChain = logging.getNextInChain();
         assertEquals(nextInChain, actual_nextInChain);
     }
